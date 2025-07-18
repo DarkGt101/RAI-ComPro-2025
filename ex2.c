d99a0a7d5715a0029d282623fbec9be31c415291
DarkGt101 <kaninpakorn.stylistic@gmail.com>
2025-07-18 14:57:22 +0000

    Complete exercise 2

---
+++ ex2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    char Firstname[] = "Kaninpakorn";
+    char Lastname[] = "Thaipadungpanit";
+    int yearold = 18;
+    int height = 170;
+    printf("My name is %s",Firstname);
+    printf(" %s", Lastname);
+    printf("\nI am %d yearold", yearold);
+    printf("\nI am %d cm tall", height);
+}
